35d2c0579971ef5de635c8ba489bc727b3508970
Riaven <jogubaez@hotmail.com>
2019-11-06 17:42:09 -0300

    añadido materializecss

--- TomaHorasMedicas/TomaHorasMedicas/settings.py
+++ TomaHorasMedicas/TomaHorasMedicas/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'materializecssform',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"))
+
