30cedfe763a3d1d6c43300a619e85d16f89fe7db
Riaven <jogubaez@hotmail.com>
2019-11-06 17:27:26 -0300

    virtualvenv

--- TomaHorasMedicas/TomaHorasMedicas/settings.py
+++ TomaHorasMedicas/TomaHorasMedicas/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'TomaHorasMedicas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"))
