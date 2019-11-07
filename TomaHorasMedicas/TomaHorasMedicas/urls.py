9d167dd340cd6b5b3df4664f39bb2fa0dd1df639
Riaven <jogubaez@hotmail.com>
2019-11-07 20:11:13 -0300

    url index funcionando

--- TomaHorasMedicas/TomaHorasMedicas/urls.py
+++ TomaHorasMedicas/TomaHorasMedicas/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
+from apps.news import views as vistaNoticias
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #Para agregar lo de las noticias
-    
+    path('inicio/', vistaNoticias.index),
 ]
--- TomaHorasMedicas/apps/news/views.py
+++ TomaHorasMedicas/apps/news/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
-# Create your views here.
+def index (request):
+    return render(request, 'principal/index.html')
