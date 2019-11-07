f8e2d8ab5b596d347f71699ea02a3d469a4f1a65
Riaven <jogubaez@hotmail.com>
2019-11-07 00:40:58 -0300

    migración profesional - área de atención correcta

--- TomaHorasMedicas/apps/profesional/admin.py
+++ TomaHorasMedicas/apps/profesional/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import AreaAtencion
 # Register your models here.
+
+#Se agrega área de atención para ser manipulada por el super usuario
+admin.site.register(AreaAtencion)
--- TomaHorasMedicas/apps/profesional/models.py
+++ TomaHorasMedicas/apps/profesional/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Área de atención
+
+class AreaAtencion (models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
