13e9f9bc0f30bcfc3b3d01ad39849cfd5d379881
Riaven <jogubaez@hotmail.com>
2019-11-24 19:58:12 -0300

    envio de cambio de contraseña fixed

--- TomaHorasMedicas/apps/login/views.py
+++ TomaHorasMedicas/apps/login/views.py
@@ -36,10 +36,9 @@ def activarCuenta(request):
                     #guarda los cambios del usuario
                     usuario.save()
                     
-                    #Asunto de activación de cuenta
-                    asunto = 'Cambio de contraseña - Activación de cuenta'
+                    
                     cambiarContrasena(correo)
-                    #enviaEmail(correo, asunto)
+                    
                 return HttpResponse('User activado :) al correo ' + correo)
                
             except Exception as e:
@@ -54,17 +53,14 @@ def activarCuenta(request):
 
 #Enviar email de recuperación de cuenta, recibe el correo al que se debe de enviar la recuperación 
 #y el asunto, ya que es de activar cuenta, tendrá otro asunto cuando sea de ha olvidado la contraseña
-def enviaEmail(correo, asunto):
-   # mensaje_html = render_to_string('login/cambiocontrasena.html')
-    contenido = strip_tags(mensaje_html)
-    envia = 'librerialmg@gmail.com'
-    
-
-    #mail.send_mail(asunto, contenido, envia, [correo]) 
-
 def cambiarContrasena(correo):
     template = 'login/cambiocontrasena.html'
+    #Se rellena el formulario con el correo del usuario que se encontro con el rut
     form = PasswordResetForm({'email' : correo})
+    #Se debe de comprobar el formulario, si no dará una exepcion
     if form.is_valid():
+        #Se guarda el formulario, enviándole un template del link para cambiar la pass
+        # se sobreescribe el dominio
+        
         return form.save(email_template_name=template, domain_override="localhost:8000")
         
