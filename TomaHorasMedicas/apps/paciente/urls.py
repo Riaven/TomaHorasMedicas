39e2243c6bfe979c17e11913b0287e5ebf1ca9b9
Riaven <jogubaez@hotmail.com>
2019-11-26 10:05:58 -0300

    fixed

--- TomaHorasMedicas/apps/paciente/urls.py
+++ TomaHorasMedicas/apps/paciente/urls.py
@@ -1,9 +1,10 @@
-from apps.paciente.views import nuevoPaciente, verFichaPropia, listarFichas, eliminarFicha, modificarPaciente
+from apps.paciente.views import nuevoPaciente, verFichaPropia, listarFichas, eliminarFicha, modificarPaciente, listarFichas
 from django.conf.urls import include, url
 
 
 urlpatterns = [
-    url(r'^$', nuevoPaciente, name='pacientes'),
+    url(r'^$', listarFichas, name='pacientes'),
+    url(r'^nuevo$', nuevoPaciente, name='nuevo_paciente'),
     url(r'^vermificha$', verFichaPropia, name='ver_mi_ficha'),
     url(r'^listapacientes$', listarFichas, name='listar_fichas'),
     url(r'^eliminar/(?P<rut>\d+)/$', eliminarFicha, name='eliminar_ficha'),
